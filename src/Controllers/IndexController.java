505c7bdfe625629fa8ad15d1d825699d7e2cb614
Endika <salgueiro93@gmail.com>
2015-11-25 15:54:54 +0100

    Se vuelve a añadir el proyecto con nuevos contenidos.

---
+++ src/Controllers/IndexController.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexController
+ */
+@WebServlet(description = "El controlador index.", urlPatterns = { "/Index" })
+public class IndexController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hola desde el metodo GET.");
+	}
+
+}
