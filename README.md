2c5ac5df72e8ef781d575ff75ffb68d708fab545
Endika <salgueiro93@gmail.com>
2016-01-18 12:03:52 +0100

    Se convierte el proyecto a Maven.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # PTSI-CityWeatherWebApp
 Proyecto de la asignatura Protocolos, Tecnologías y Servicios de Internet que consistirá en una aplicación web para consultar pronósticos meteorológicos y que le permita al usuario guardar ciudades favoritas para recibir alertas sobre las previsiones.
+
+PASOS PARA COMPILAR, PROBAR Y SUBIR EL PROYECTO A GOOGLE APP ENGINE
+-------------------------------------------------------------------
+1. Pasos para compilar el proyecto GAE, que hace uso de Swagger y Jersey:
+	mvn clean 
+	mvn -Dmaven.test.skip=true package
+	mvn -Dmaven.test.skip=true appengine:devserver
+2. Para ejecutar el servicio:
+	a. Copiar el fichero CityWeatherWebApp.war generado en carpeta target a webapps en Tomcat
+	b. Lanzar Tomcat: catalina.bat run
+3. Para probar el servicio:
+	- Ir a un navegador y probar el servicio RESTful:
+		a. http://localhost:8080/
+		b. http://localhost:8080/CityWeatherWebApp/index
+4. Para subir la aplicación a Google App Engine	
+	a. Ir Google Cloud Platform Developer Console: https://console.cloud.google.com/home/dashboard
+	b. Crear un nombre de app: CityWeatherWebApp
+	c. Actualizar el fichero CityWeatherWebApp\src\main\webapp\WEB-INF\appengine-web.xml
+		- Campo <application>CityWeatherWebApp</application>
+	d. Ejecutar el comando en el directorio donde está pom.xml: 
+		mvn -Dmaven.test.skip=true appengine:update
+	e. Probar la aplicación yendo a: http://CityWeatherWebApp.appspot.com/. 
+
+Explicación de error que se producía al subir la app
+----------------------------------------------------
+
+Se producía un error como el siguiente:
+loading application configuration:Unable to assign value does not match expression '^(?:^(\-]{0,62}[a-z\d]$)$'
+
+La respuesta más votada en: http://stackoverflow.com/questions/18239234/bad-request-when-updating-appengine-with-mvn-appengineupdate
+
+Es la solución al problema.
---
+++ src/main/java/Conversion.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2014-2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//[START all]
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Parent;
+import com.googlecode.objectify.Key;
+
+import java.lang.String;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * The @Entity tells Objectify about our entity.  We also register it in {@link OfyHelper}
+ * Our primary key @Id is set automatically by the Google Datastore for us.
+ *
+ * Objectify, unlike the AppEngine library requires that you specify the fields you
+ * want to index using @Index.  Only indexing the fields you need can lead to substantial gains in
+ * performance -- though if not indexing your data from the start will require indexing it later.
+ *
+ * NOTE - all the properties are PUBLIC so that can keep the code simple.
+ **/
+@Entity
+public class Conversion {
+  @Id public Long id;
+  public String type;
+  public String value;
+  public String result;
+  @Index public Date date;
+
+  /**
+   * Simple constructor just sets the date
+   **/
+  public Conversion() {
+    date = new Date();
+  }
+
+  /**
+   * A connivence constructor
+   **/
+  public Conversion(String type, String value, String result) {
+    this();
+    this.type = type;
+	this.value = value;
+	this.result = result;
+  }
+}
+//[END all]
